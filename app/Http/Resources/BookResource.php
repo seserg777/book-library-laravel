85c25b0b866181a88c8338923514850397a410ca
seserg777 <seserg777@gmail.com>
2026-04-23 13:10:32 +0200

    Add home page with books list. Add page to add new book with form.

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Book
+ */
+class BookResource extends JsonResource
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'publisher' => $this->publisher,
+            'author' => $this->author,
+            'genre' => $this->genre,
+            'publication_date' => $this->publication_date?->toDateString(),
+            'word_count' => $this->word_count,
+            'price_usd' => (string) $this->price_usd,
+            'created_at' => $this->created_at?->toIso8601String(),
+            'updated_at' => $this->updated_at?->toIso8601String(),
+        ];
+    }
+}
